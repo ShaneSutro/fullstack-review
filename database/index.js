8d225a359d7d104f24f6c6389fe9a51f96771582
Shane Sutro <shanesutro@gmail.com>
2021-02-10 19:19:55 -0700

    Adjusting for environment variables

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/fetcher', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fetcher', {
   useCreateIndex: true,
   autoIndex: true
 });
